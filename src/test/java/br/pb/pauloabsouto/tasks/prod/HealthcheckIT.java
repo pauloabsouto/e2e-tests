9297ff8efd714ddf0e9c61bcf4fd996ad92f7ae4
pauloabsouto <pauloabsouto@gmail.com>
2020-08-27 17:04:14 -0300

    Adição do Healthcheck.

---
+++ src/test/java/br/pb/pauloabsouto/tasks/prod/HealthcheckIT.java
@@ -0,0 +1,31 @@
+package br.pb.pauloabsouto.tasks.prod;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class HealthcheckIT {
+
+    @Test
+    public void healthCheck() throws MalformedURLException {
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.4:4444/wd/hub"), cap);
+        try{
+            driver.navigate().to("http://192.168.1.4:9999/tasks/");
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            String version = driver.findElement(By.id("version")).getText();
+            Assert.assertTrue(version.startsWith("build"));
+        } finally {
+            driver.quit();
+        }
+
+
+    }
+}
